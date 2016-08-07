6f3ade12bc1da3cce8a074796cb244a537f4c191
Krzysztof Klis <krzysztof.klis@allegrogroup.com>
2016-08-07 23:55:17 +0200

    Fixed assembly example

--- ArduinoInterface/arduino.asm
+++ ArduinoInterface/arduino.asm
@@ -1,12 +1,11 @@
 org 100h
-LD C,220
-LD D,13
-LD E,1
-CALL 5
-LD C,222
-LD D,13
-LD E,1
-CALL 5
-RET
-END
-
+mvi c,220
+mvi d,53
+mvi e,1
+call 5
+mvi c,222
+mvi d,53
+mvi e,1
+call 5
+ret
+
